1879bfdb8bc4ca66a0df3fa2330cbde5c70c21be
geisel0 <gpachecome@unsa.edu.pe>
2024-12-09 21:21:05 -0500

    clase arquero modificada

--- src/Arquero.java
+++ src/Arquero.java
@@ -1,31 +1,15 @@
 public class Arquero extends Soldado {
     private int flechasDisponibles;
-    private static int contador;
 
-
-    public Arquero() {
-        super();
-        setNombre("Arquero" + contador);
-        setNivelAtaque(4);
-        setNivelDefensa(1);
-        flechasDisponibles = 10; // Número inicial de flechas
-    }
-
-    public void disparar(Soldado objetivo) {
-        if (flechasDisponibles > 0) {
-            System.out.println(getNombre() + " dispara una flecha.");
-            flechasDisponibles--;
-            atacar(objetivo);
-        } else {
-            System.out.println(getNombre() + " se ha quedado sin flechas.");
-        }
-    }
-
-    public int getFlechasDisponibles() {
-        return flechasDisponibles;
-    }
-
-    public void setFlechasDisponibles(int flechasDisponibles) {
+    public Arquero(String nombre, int nivelVida, int flechasDisponibles) {
+        super(nombre, nivelVida, 7, 3);
         this.flechasDisponibles = flechasDisponibles;
+    }
+
+    @Override
+    public void realizarAccionEspecial() {
+        if (flechasDisponibles > 0) {
+            flechasDisponibles--;
+        }
     }
 }
