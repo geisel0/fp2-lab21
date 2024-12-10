b5f69d270ab2a3f965984a11cd3029080b63a41e
geisel0 <gpachecome@unsa.edu.pe>
2024-12-09 21:38:39 -0500

    clase caballero modificada

--- src/Caballero.java
+++ src/Caballero.java
@@ -1,50 +1,22 @@
 public class Caballero extends Soldado {
-    private String armaActual; // "espada" o "lanza"
     private boolean montado;
-    private static int contador;
 
-    public Caballero() {
-        super();
-        setNombre("Caballero" + contador);
-        setNivelAtaque(5);
-        setNivelDefensa(3);
-        armaActual = "lanza";
-        montado = true; // Inicia montado por defecto
+    public Caballero(String nombre, int nivelVida) {
+        super(nombre, nivelVida, 13, 7);
+        this.montado = true;
     }
 
-    public void montar() {
-        if (!montado) {
-            montado = true;
-            armaActual = "lanza";
-            System.out.println(getNombre() + " monta y cambia a lanza.");
-        } else {
-            System.out.println(getNombre() + " ya está montado.");
-        }
-    }
-
-    public void desmontar() {
+    @Override
+    public void realizarAccionEspecial() {
         if (montado) {
-            montado = false;
-            armaActual = "espada";
-            System.out.println(getNombre() + " desmonta y cambia a espada.");
+            nivelAtaque += 5;
         } else {
-            System.out.println(getNombre() + " ya está desmontado.");
+            nivelDefensa += 3;
         }
     }
 
-    public void envestir(Soldado objetivo) {
-        System.out.println(getNombre() + " realiza una envestida.");
-        int ataques = montado ? 3 : 2;
-        for (int i = 0; i < ataques; i++) {
-            atacar(objetivo);
-        }
-    }
-
-    public String getArmaActual() {
-        return armaActual;
-    }
-
-    public boolean isMontado() {
-        return montado;
+    public void alternarMontura() {
+        montado = !montado;
+        nivelAtaque = montado ? 13 : 10;
     }
 }
