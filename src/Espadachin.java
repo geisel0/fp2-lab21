0a4e0a3b5f808fc83da8f7c57e05c00809116e54
geisel0 <gpachecome@unsa.edu.pe>
2024-12-09 21:13:14 -0500

    clase espadachin modificada

--- src/Espadachin.java
+++ src/Espadachin.java
@@ -1,24 +1,13 @@
 public class Espadachin extends Soldado {
     private int longitudEspada;
-    private static int contador=0;
 
-    public Espadachin() {
-        super();
-        setNombre("Espadachin" + contador);
-        setNivelAtaque(3);
-        setNivelDefensa(2);
-        longitudEspada = 100; // Longitud en centímetros
-    }
-
-    public void muroDeEscudos() {
-        System.out.println(getNombre() + " crea un muro de escudos.");
-    }
-
-    public int getLongitudEspada() {
-        return longitudEspada;
-    }
-
-    public void setLongitudEspada(int longitudEspada) {
+    public Espadachin(String nombre, int nivelVida, int longitudEspada) {
+        super(nombre, nivelVida, 10, 8);
         this.longitudEspada = longitudEspada;
+    }
+
+    @Override
+    public void realizarAccionEspecial() {
+        nivelDefensa += 2;
     }
 }
