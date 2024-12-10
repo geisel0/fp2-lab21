0c5b9351d07d82f32f5ba4f966c0965dad1ed3b9
geisel0 <gpachecome@unsa.edu.pe>
2024-12-09 23:50:18 -0500

    main modificado

--- src/App.java
+++ src/App.java
@@ -1,82 +1,23 @@
-import java.util.Random;
-import java.util.Scanner;
-
-
 public class App {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Tablero tablero = new Tablero();
-        Ejercito ejercito1 = new Ejercito("Rojo");
-        Ejercito ejercito2 = new Ejercito("Azul");
+        Ejercito ejercito1 = new Ejercito("Inglaterra");
+        Ejercito ejercito2 = new Ejercito("Francia");
 
-        generarEjercito(tablero, ejercito1, 'R');
-        generarEjercito(tablero, ejercito2, 'B');
-
-        boolean juegoActivo = true;
-        while (juegoActivo) {
-            System.out.println("Estado del tablero:");
-            tablero.mostrarTablero();
-
-            System.out.println("Turno del jugador 1 (Ejército Rojo).");
-            turnoJugador(tablero, scanner, ejercito1);
-
-            if (ejercito2.getSoldados().isEmpty()) {
-                System.out.println("¡El jugador 1 (Ejército Rojo) ganó!");
-                break;
-            }
-
-            System.out.println("Turno del jugador 2 (Ejército Azul).");
-            turnoJugador(tablero, scanner, ejercito2);
-
-            if (ejercito1.getSoldados().isEmpty()) {
-                System.out.println("¡El jugador 2 (Ejército Azul) ganó!");
-                break;
-            }
+        for (int i = 0; i < 5; i++) {
+            ejercito1.agregarSoldado(new Espadachin("Espadachin" + i, 9, 5));
+            ejercito2.agregarSoldado(new Arquero("Arquero" + i, 4, 10));
         }
-    }
 
-    public static void generarEjercito(Tablero tablero, Ejercito ejercito, char simbolo) {
-        Random random = new Random();
-        for (int i = 0; i < 10; i++) {
-            int fila = random.nextInt(10);
-            int columna = random.nextInt(10);
+        Mapa mapa = new Mapa(10, 10, "bosque");
+        mapa.aplicarBonus(ejercito1);
 
-            while (tablero.posicionOcupada(fila, columna)) {
-                fila = random.nextInt(10);
-                columna = random.nextInt(10);
-            }
+        Tablero tablero = mapa.getTablero();
+        ejercito1.getSoldados().forEach(tablero::posicionarSoldado);
+        ejercito2.getSoldados().forEach(tablero::posicionarSoldado);
 
-            Soldado soldado;
-            int tipoSoldado = random.nextInt(3);
-            if (tipoSoldado == 0) {
-                soldado = new Espadachin();
-            } else if (tipoSoldado == 1) {
-                soldado = new Arquero();
-            } else {
-                soldado = new Caballero();
-            }
+        tablero.mostrarTablero();
 
-            soldado.setSimbolo(simbolo);
-            tablero.colocarSoldado(soldado, fila, columna);
-        }
-    }
-
-    public static void turnoJugador(Tablero tablero, Scanner scanner, Ejercito ejercito) {
-        System.out.println("Ingrese fila y columna del soldado a mover:");
-        int filaInicial = scanner.nextInt();
-        int columnaInicial = scanner.nextInt();
-
-        System.out.println("Ingrese dirección de movimiento (arriba/abajo/izquierda/derecha):");
-        String direccion = scanner.next();
-
-        int filaDestino = filaInicial;
-        int columnaDestino = columnaInicial;
-
-        if (direccion.equals("arriba")) filaDestino--;
-        else if (direccion.equals("abajo")) filaDestino++;
-        else if (direccion.equals("izquierda")) columnaDestino--;
-        else if (direccion.equals("derecha")) columnaDestino++;
-
-        tablero.moverSoldado(filaInicial, columnaInicial, filaDestino, columnaDestino);
+        System.out.println("Poder total de Inglaterra: " + ejercito1.calcularPoderTotal());
+        System.out.println("Poder total de Francia: " + ejercito2.calcularPoderTotal());
     }
 }
